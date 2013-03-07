0b36509381c7b805878245d1d95190cb4227f419
AmeliaMN <amelia.a.mcnamara@gmail.com>
2013-03-06 19:15:33 -0800

    Respecified the function to work specifically with dynamically-generated extents, and modified the documentation to match.

--- R/SpatialSubset.R
+++ R/SpatialSubset.R
@@ -1,5 +1,6 @@
 SpatialSubset <- function(latitude, longitude, e) {
   xy1 <- cbind(longitude, latitude)
-  subset1 <- which(xy1[,2]<ymax(e) & xy1[,2]>ymin(e) & xy1[,1]<xmax(e) & xy1[,1]>xmin(e))
+  projected <- Mercator(xy1)
+  subset1 <- which(projected[,2]<ymax(e) & projected[,2]>ymin(e) & projected[,1]<xmax(e) & projected[,1]>xmin(e))
   return(i=subset1)
 }
