497e82fe72d2173f24761f57a6d8e330d2dbef39
JimmyLovesTea <jmolyneux@ucla.edu>
2014-01-07 00:23:08 -0800

    Included 'top' argument

--- R/MakeWordBar.R
+++ R/MakeWordBar.R
@@ -1,4 +1,4 @@
-MakeWordBar <- function(text, min.freq=2, ...){
+MakeWordBar <- function(text, min.freq=2, top = 100, ...){
   if (class(text)[1]!="VCorpus"){
     stop("Remember to initialize text using initializeText()")
   }
@@ -7,12 +7,30 @@ MakeWordBar <- function(text, min.freq=2, ...){
   m <- as.matrix(tdm)
   v <- sort(rowSums(m), decreasing=T)
   d <- data.frame(word=names(v), freq=v, row.names=NULL)
-  d<- d[d$freq>=min.freq,]
-  largest.word <- max(nchar(as.character(d$word)))
-  if (largest.word > 8) {
-    adj = (largest.word - 7) / 3
-    par(mar = old.par + c(adj, 0, 0 , 0))
+  d <- d[d$freq>=min.freq,]
+  if (top >= 1) {
+    top.words <- head(d$freq, n = top)
+    top.words.names <- head(d$word, n = top)
+    largest.word <- max(nchar(as.character(top.words.names)))
+    if (largest.word > 8) {
+      adj = (largest.word - 7) / 3
+      par(mar = old.par + c(adj, 0, 0, 0))
+    } else {
+      par(mar = old.par)
+    }
   }
-  barplot(d$freq, names.arg=d$word, las = 2, ...)
+  if (top < 1) {
+    n <- floor(nrow(d) * top)
+    top.words <- head(d$freq, n = n)
+    top.words.names <- head(d$word, n = n)
+    largest.word <- max(nchar(as.character(top.words.names)))
+    if (largest.word > 8) {
+      adj = (largest.word - 7) / 3
+      par(mar = old.par + c(adj, 0, 0, 0))
+    } else {
+      par(mar = old.par)
+    }    
+  }  
+  barplot(top.words, names.arg=top.words.names, las = 2, ...)
   par(mar = old.par)
 }
