10d94a63d81ac2b7317e5f85e4600ca97d785265
JimmyLovesTea <jmolyneux@ucla.edu>
2014-07-17 19:43:08 -0700

    Added footsize reference and footsize format function

---
+++ R/footsize_format.R
@@ -0,0 +1,21 @@
+footsize_format <- function(data) {
+    require(dplyr)
+    # data(foot_size_ref)
+    data(women_footsize_ref)
+    df <- data %.% select(user.id, 
+                          birth_gender = birth_gender.label,
+                          shoe_size = shoe_size.value, 
+                          shoe_type = size_type.label)
+    
+    df <- inner_join(df, women_footsize_ref, by = "shoe_size")
+    
+    df <- df %.% select(user.id, 
+                        birth_gender,
+                        shoe_size, 
+                        shoe_type, 
+                        foot_inches = women_inches) 
+    
+    df$foot_inches[which(df$shoe_type == "Mens")] <- df$foot_inches[which(df$shoe_type == "Mens")] + 0.5
+    data <- df
+    return(data)
+}
