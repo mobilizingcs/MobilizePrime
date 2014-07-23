30847c0cb7e00ec0173344fb79f4218489951abf
JimmyLovesTea <jmolyneux@ucla.edu>
2014-07-23 01:15:37 -0700

    add_line function now prints the equation of the line

--- R/add_line.R
+++ R/add_line.R
@@ -1,6 +1,6 @@
 add_line <- function(units = "native") {
     trellis.focus("panel", 1, 1)
-    cat("Select 2 data points to draw a line through:")
+    cat("Select 2 data points to draw a line through: \n")
     ind1_raw <- grid.locator(unit = "native")
     ind2_raw <- grid.locator(unit = "native")
     ind1 <- as.numeric(ind1_raw)
@@ -8,4 +8,7 @@ add_line <- function(units = "native") {
     b <- (ind2[2] - ind1[2])/(ind2[1] - ind1[1])
     a <- ind1[2] - b*ind1[1]
     ladd(panel.abline(a=a, b=b))
+    cat("Equation of the line: \n")
+    cat("y = ", b, "* x + ", a)
 }
+add_line()
