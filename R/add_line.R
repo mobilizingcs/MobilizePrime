deaa0d2192aed275fa0279af87a46cda1e411001
JimmyLovesTea <jmolyneux@ucla.edu>
2014-07-23 01:10:30 -0700

    Added the add_line function

---
+++ R/add_line.R
@@ -0,0 +1,11 @@
+add_line <- function(units = "native") {
+    trellis.focus("panel", 1, 1)
+    cat("Select 2 data points to draw a line through:")
+    ind1_raw <- grid.locator(unit = "native")
+    ind2_raw <- grid.locator(unit = "native")
+    ind1 <- as.numeric(ind1_raw)
+    ind2 <- as.numeric(ind2_raw)    
+    b <- (ind2[2] - ind1[2])/(ind2[1] - ind1[1])
+    a <- ind1[2] - b*ind1[1]
+    ladd(panel.abline(a=a, b=b))
+}
