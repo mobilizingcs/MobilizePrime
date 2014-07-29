31b3793f5f420ade9b9cae71fd6b5e96911f0d88
JimmyLovesTea <jmolyneux@ucla.edu>
2014-07-28 22:30:54 -0700

    Even more troubleshooting

--- R/format_lab_title.R
+++
@@ -1,5 +0,0 @@
-format_lab_title <- function (x) {
-    x_lower <- tolower(x)
-    x_nospace <- gsub(x = x_lower, pattern = ' ', replacement = "")
-    gsub(x = x_nospace, pattern = "-", replacement = "")
-}
--- R/lab_selector.R
+++
@@ -1,8 +0,0 @@
-lab_selector <- function (lab_titles) {
-    selection <- utils::menu(lab_titles)
-    lab_urls <- paste0('http://www.stat.ucla.edu/~james.molyneux/',
-                       format_lab_title(lab_titles),
-                       '.html')
-    url <- lab_urls[selection]
-    return(url)
-}
