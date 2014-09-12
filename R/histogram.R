5ebcdcd32ed93a27d67f330f34bddfb822090c62
JimmyLovesTea <jmolyneux@ucla.edu>
2014-09-12 13:45:32 -0700

    Troubleshooting histogram bugs

--- R/histogram.R
+++
@@ -1,9 +0,0 @@
-### Change default unit of y-axis to 'count'
-
-histogram <- function(..., fit) {
-  if (missing(fit)) {
-    lattice::histogram(..., type = 'count')
-  } else {
-    lattice::histogram(..., fit = fit)
-  }
-}
