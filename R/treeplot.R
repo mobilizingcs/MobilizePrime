14d536c12037c11e70e5d7534b5f2b96d4182bc3
JimmyLovesTea <jmolyneux@ucla.edu>
2015-03-19 17:56:55 -0700

    Stop allowing users to plot tree diagrams with greater than 20 branches

--- R/treeplot.R
+++ R/treeplot.R
@@ -1,3 +1,15 @@
-treeplot <- function(...) {
-  rpart.plot::prp(...,type=3, extra=3, under=TRUE, clip.right.labs=FALSE,varlen=0, faclen=0)
+tree_splits <- function(x) {
+  cp_table <- x["cptable"]
+  cp_df <- as.data.frame(cp_table)
+  splits <- cp_df$cptable.nsplit
+  return(splits)
+}
+
+treeplot <- function(model, ...) {
+  if(class(model) == "rpart") {
+    n_splits <- tree_splits(x=model)
+    max_splits <- base::max(n_splits)
+  }
+  if (max_splits > 20) stop("Treeplot is too complex to plot")
+  rpart.plot::prp(x=model, type=3, extra=3, under=TRUE, clip.right.labs=FALSE,varlen=0, faclen=0, ...)
 }
